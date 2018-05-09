c4f708ffa64b44924d36a907c35c312a3ec333df
Katherine Beame <kbeame@uw.edu>
2018-05-09 15:39:11 -0700

    change font size and direction depending on tile size

--- src/components/treemap.tsx
+++ src/components/treemap.tsx
@@ -1,11 +1,8 @@
-import {
-  treemap,
-  treemapResquarify,
-} from 'd3';
+import { treemap, treemapResquarify } from 'd3';
+import { max } from "d3-array";
 import React from 'react';
 
 // Eventually handle dynamically with reference to space available
-const FONT_SIZE = 14;
 
 interface TreemapFieldAccessors {
   label: string;
@@ -19,7 +16,7 @@ interface LayoutOptions {
 
 interface TreemapProps {
   colorScale: (input: number | string) => string;
-  data: any,
+  data: any;
   fieldAccessors: TreemapFieldAccessors;
   height: number;
   showToDepth: number;
@@ -27,11 +24,11 @@ interface TreemapProps {
   strokeWidth: number | string;
   layoutOptions: LayoutOptions;
   width: number;
-};
+}
 
 interface TreemapState {
-  layout: any,
-};
+  layout: any;
+}
 
 export default class Treemap extends React.Component<
   TreemapProps,
@@ -50,7 +47,7 @@ export default class Treemap extends React.Component<
 
   constructor(props) {
     super(props);
-    this.state = { layout: this.getLayout() }
+    this.state = { layout: this.getLayout() };
   }
 
   getLayout = () => {
@@ -80,19 +77,62 @@ export default class Treemap extends React.Component<
       width={d.x1 - d.x0}
       height={d.y1 - d.y0}
     />
-  ));
+  ))
+
+    stringWidth = (stringThing, size = 12) => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        if (context) {
+            context.font = `${size}px Verdana`;
+            return Math.ceil(context.measureText(stringThing).width);
+        }
+        return 0;
+    }
+
+    _fontSize = d => {
+        const minValue = 10;
+        const maxValue = 48;
+
+        const x = max([0, Math.floor(((d.x1 - d.x0) - 8))]);
+        const y = max([0, Math.floor(((d.y1 - d.y0) - 8))]);
+
+        let size;
+        const label = d.data.location_name;
+        const width = (this.stringWidth(label, 12) / 12);
+        const direction = ((y > x && (width * (minValue + 1)) > x) ? 'tb' : 'lr');
+
+        if (direction === 'lr') { size = (y < (x / width)) ? y : (x / width); }
+        if (direction === 'tb') { size = (x < (y / width)) ? x : (y / width); }
+
+        if (size < minValue) { size = 0; }
+        if (size >= maxValue) { size = maxValue; }
+
+        return size;
+    }
+
+    _fontDirection = d => {
+        const min = 10;
+        const x = max([0, Math.floor(((d.x1 - d.x0) - 8))]);
+        const y = max([0, Math.floor(((d.y1 - d.y0) - 8))]);
+        const label = d.data.location_name;
+        const width = this.stringWidth(label, 12) / 12;
+        const direction = (y > x && (width * (min + 1)) > x) ? 'tb' : 'lr';
+        const lr = `translate(3px, ${this._fontSize(d)}px) rotate(0)`;
+        const tb = `translate(${this._fontSize(d) / 3}px, 3px) rotate(90deg)`;
+        return (direction === 'lr') ? lr : tb;
+    }
 
   renderText = (d) => ((
     <text
       key={`text-${d.id}`}
       style={{
-        fontSize: FONT_SIZE,
-        transform: `translate(3px, ${FONT_SIZE}px)`,
+        fontSize: this._fontSize(d),
+        transform: this._fontDirection(d),
       }}
     >
       {d.data[this.props.fieldAccessors.label]}
     </text>
-  ));
+  ))
 
   renderCell = d => ((
     <g
@@ -102,7 +142,7 @@ export default class Treemap extends React.Component<
       {this.renderRect(d)}
       {this.renderText(d)}
     </g>
-  ));
+  ))
 
   render() {
     const {
