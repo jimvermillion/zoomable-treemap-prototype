0598c8eda3ba707f6012c29c33e272005f209d5e
Katherine Beame <kbeame@uw.edu>
2018-05-10 09:41:51 -0700

    small style fixes and create constants for number clarity

--- src/components/treemap.tsx
+++ src/components/treemap.tsx
@@ -1,8 +1,14 @@
-import { treemap, treemapResquarify } from 'd3';
-import { max } from "d3-array";
+import {
+  treemap,
+  treemapResquarify,
+} from 'd3';
+import { max } from 'd3-array';
 import React from 'react';
 
 import { stringWidth } from '../utils';
+
+const FONT_SIZE_DEFAULT = 12;
+const FONT_PADDING_DEFAULT = 8;
 
 // Eventually handle dynamically with reference to space available
 
@@ -83,34 +89,56 @@ export default class Treemap extends React.Component<
     />
   ))
 
-    sizingProperties = d => {
-        const [ minValue, maxValue ] = this.props.fontSize;
-        const x = max([0, Math.floor(((d.x1 - d.x0) - 8))]);
-        const y = max([0, Math.floor(((d.y1 - d.y0) - 8))]);
-        const label = d.data.location_name;
-        const width = stringWidth(label, 12) / 12;
-        const direction = (y > x && (width * (minValue + 1)) > x) ? 'topBottom' : 'leftRight';
-        return { x, y, width, direction, minValue, maxValue };
-    }
+  sizingProperties = d => {
+    const [minValue, maxValue] = this.props.fontSize;
+    const x = max([0, Math.floor(((d.x1 - d.x0) - FONT_PADDING_DEFAULT))]);
+    const y = max([0, Math.floor(((d.y1 - d.y0) - FONT_PADDING_DEFAULT))]);
+    const label = d.data.location_name;
+    const width = stringWidth(label, FONT_SIZE_DEFAULT) / FONT_SIZE_DEFAULT;
+    const direction = (y > x && (width * (minValue + 1)) > x) ? 'topBottom' : 'leftRight';
+    return {
+      x,
+      y,
+      width,
+      direction,
+      minValue,
+      maxValue,
+    };
+  }
 
-    fontSize = d => {
-        const { x, y, width, direction, minValue, maxValue } = this.sizingProperties(d);
-        let size;
-        if (direction === 'leftRight') { size = (y < (x / width)) ? y : (x / width); }
-        if (direction === 'topBottom') { size = (x < (y / width)) ? x : (y / width); }
-        if (size < minValue) { size = 0; }
-        if (size >= maxValue) { size = maxValue; }
-        return size;
+  fontSize = d => {
+    const {
+      x,
+      y,
+      width,
+      direction,
+      minValue,
+      maxValue,
+    } = this.sizingProperties(d);
+    let size;
+    if (direction === 'leftRight') {
+      size = (y < (x / width)) ? y : (x / width);
     }
+    if (direction === 'topBottom') {
+      size = (x < (y / width)) ? x : (y / width);
+    }
+    if (size < minValue) {
+      size = 0;
+    }
+    if (size >= maxValue) {
+      size = maxValue;
+    }
+    return size;
+  }
 
-    fontDirection = d => {
-        const { direction } = this.sizingProperties(d);
-        const orientation = {
-            leftRight: `translate(3px, ${this.fontSize(d)}px) rotate(0)`,
-            topBottom: `translate(${this.fontSize(d) / 3}px, 3px) rotate(90deg)`,
-        };
-        return (direction === 'leftRight') ? orientation.leftRight : orientation.topBottom;
-    }
+  fontDirection = d => {
+    const { direction } = this.sizingProperties(d);
+    const orientation = {
+      leftRight: `translate(3px, ${this.fontSize(d)}px) rotate(0)`,
+      topBottom: `translate(${this.fontSize(d) / 3}px, 3px) rotate(90deg)`,
+    };
+    return (direction === 'leftRight') ? orientation.leftRight : orientation.topBottom;
+  }
 
   renderText = (d, dropshadow = '') => ((
     <text
