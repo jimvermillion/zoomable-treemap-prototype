96bb637fd75390358203f5fb391a4539c4bfe6b7
James Vermillion <vermijw@uw.edu>
2018-05-16 10:31:07 -0700

    Update Rectangle.tsx with attribution changes.

--- src/components/Rectangle.tsx
+++ src/components/Rectangle.tsx
@@ -4,16 +4,25 @@ interface RectangleProps {
   data: any;
   fill: string;
   height: number;
-  stroke: string;
-  strokeWidth: number | string;
+  stroke?: string;
+  strokeWidth?: number | string;
   onClick: (...args: any[]) => void;
   onMouseOver: (...args: any[]) => void;
   onMouseLeave: (...args: any[]) => void;
   onMouseMove: (...args: any[]) => void;
   width: number;
+  opacity?: number;
+  transform?: string;
 }
 
 export default class Rectangle extends React.PureComponent<RectangleProps> {
+  static defaultProps: Partial<RectangleProps> = {
+    stroke: null,
+    strokeWidth: null,
+    transform: null,
+    opacity: null,
+  };
+
   onClick = event => {
     const {
       data,
@@ -57,15 +66,19 @@ export default class Rectangle extends React.PureComponent<RectangleProps> {
       stroke,
       width,
       height,
+      transform,
+      opacity,
     } = this.props;
 
     return (
       <rect
+        transform={transform}
         fill={fill}
         strokeWidth={strokeWidth}
         stroke={stroke}
         width={width}
         height={height}
+        opacity={opacity}
         onClick={this.onClick}
       />
     );
