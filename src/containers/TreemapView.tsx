bed821227c497100bcb46a451a2afa3ccbba70d1
James Vermillion <vermijw@uw.edu>
2018-05-16 10:36:37 -0700

    Add Kat's changes on fieldAccessors to TreemapView.tsx.

--- src/containers/TreemapView.tsx
+++ src/containers/TreemapView.tsx
@@ -85,7 +85,7 @@ export default class TreemapView extends React.PureComponent<
       <Treemap
         data={data}
         rootNodeId={rootNodeId}
-        fieldAccessors={{ label: 'location_name' }}
+        fieldAccessors={{ label: 'location_name', attribution: { name: 'attribution', fill: 'fill' } }}
         showToDepth={showToDepth}
         onClick={this.zoomIn}
         onDoubleClick={this.zoomOut}
