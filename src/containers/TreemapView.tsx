2e79e8be7789004f27dea2c602db5528d4931320
James Vermillion <vermijw@uw.edu>
2018-05-23 11:22:13 -0700

    Improve: Update zoom/zoom-out rules for leaf nodes with siblings.

--- src/containers/TreemapView.tsx
+++ src/containers/TreemapView.tsx
@@ -33,30 +33,50 @@ export default class TreemapView extends React.PureComponent<
     this.state = { showToDepth: 3 };
   }
 
+  getParents = (lilNode) => {
+    if (!lilNode.parent) {
+      return [lilNode.data.location_name];
+    }
+    return [...this.getParents(lilNode.parent), lilNode.data.location_name];
+  }
+
   zoomOut = (_, node) => {
-    // Get zoom-out `depth`
-    const showToDepth = (
-      // If node is deeper than the root:
-      node.depth > 0
-      // Ascend 1 in the `depth` tree.
-      ? node.depth - 1
-      // Otherwise, use current depth.
-      : this.state.showToDepth
+    // Is the Treemap at a leaf node that has siblings?
+    const zoomedAsFarAsPossibleOnNonOnlyChild = (
+      node.height === 0
+      && node.id === this.state.rootNodeId
+      && node.parent.children.length > 1
     );
 
     // Go to grandparent, parent, or self if cannot zoom out further.
-    const { id: rootNodeId } = (
+    const rootNode = (
       // Use grand parent.
-      (node.parent && node.parent.parent)
+      (!zoomedAsFarAsPossibleOnNonOnlyChild && node.parent && node.parent.parent)
       // Use parent if no grand parent.
       || node.parent
       // Use self if there are no ancestors.
       || node
     );
 
+    const depthToAscend = (
+      zoomedAsFarAsPossibleOnNonOnlyChild
+      ? 0
+      : 1
+    );
+
+    // Get zoom-out `depth`
+    const showToDepth = (
+      // If node is deeper than the root:
+      node.depth > 0
+      // Ascend 1 in the `depth` tree.
+      ? node.depth - depthToAscend
+      // Otherwise, use current depth.
+      : this.state.showToDepth
+    );
+
     // Update state.
     this.setState({
-      rootNodeId,
+      rootNodeId: rootNode.id,
       showToDepth,
     });
   }
